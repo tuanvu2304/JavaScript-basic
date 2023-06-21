c0c1c1fc7162f3f65cc9beae906a574601feb313
tuanvu2304 <truongtuanvu2304@gmail.com>
2023-06-21 21:57:23 +0700

    source chapter 2, bai3

--- bai2/bai2.js
+++
@@ -1,2 +0,0 @@
-console.log('Hello World From JavaScript')
-console.log('Hello World With TomTom')
---
+++ bai3/bai3.js
@@ -0,0 +1,6 @@
+console.log(`Hello World From JavaSCript`)
+
+let x = 5, y = 6, z = 7;
+
+console.log("Hello World From Strings", 'x =', x , 'y =' , y , 'z =' , z)
+
